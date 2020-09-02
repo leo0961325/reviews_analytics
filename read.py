5b58d220739dccfeb528ce485cefed03ba71a023
leo <leo09613257@gmail.com>
2020-09-03 00:10:35 +0800

    second version

--- read.py
+++ read.py
@@ -6,8 +6,11 @@ with open("reviews.txt" , "r") as f :
         data.append(line) #每一行line 加入至data裡面
         count += 1
         if count % 1000 == 0 :
-            print(len(data)) #找進度
-print(len(data)) #求筆數
-
-
+            print(len(data)) #找進度，每1000回報一次
+print(len(data)) #求總共筆數
 print(data[0]) #第幾筆
+
+sum_len = 0
+for d in data :
+    sum_len += len(d)
+print("平均是" , sum_len / len(data) , "每一筆") #平均每一筆留言字數
